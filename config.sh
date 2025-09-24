f1f3943930b3ce5081c856042c89e73ee3f7bab1
scriptkiddie <scriptkiddie@kali.kali>
2025-09-24 11:49:11 +0530

    added wallpaper

--- config.sh
+++ config.sh
@@ -11,10 +11,10 @@ mkdir -p ~/.config/polybar/scripts
 mkdir -p ~/.config/i3
 
 # Move config files to their respective folders
-mv -v alarcitty.toml ~/.config/alarcitty/
+mv -v alacritty.toml ~/.config/alarcitty/
 mv -v polybar-config.ini ~/.config/polybar/config.ini
 mv -v picom.conf ~/.config/picom/
 mv -v config ~/.config/i3/
 mv -v polybar-script-tun0-ip.sh ~/.config/polybar/scripts/tun0-ip.sh
 mv -v tmux.conf ~/.tmux.conf
- 
+mv -v wall-1.jpg  ~/Pictures/wall-1.jpg
