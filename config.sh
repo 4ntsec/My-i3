b930ac995efb2432f3b67409ce10f97a5afc191c
scriptkiddie <scriptkiddie@kali.kali>
2025-09-24 11:32:10 +0530

    add installation script

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt update
+sudo apt install -y alacritty rofi picom polybar i3 tmux
+
+# Create required directories
+mkdir -p ~/.config/alarcitty
+mkdir -p ~/.config/picom
+mkdir -p ~/.config/polybar/scripts
+mkdir -p ~/.config/i3
+
+# Move config files to their respective folders
+mv -v alarcitty.toml ~/.config/alarcitty/
+mv -v polybar-config.ini ~/.config/polybar/config.ini
+mv -v picom.conf ~/.config/picom/
+mv -v config ~/.config/i3/
+mv -v polybar-script-tun0-ip.sh ~/.config/polybar/scripts/tun0-ip.sh
+mv -v tmux.conf ~/.tmux.conf
+ 
